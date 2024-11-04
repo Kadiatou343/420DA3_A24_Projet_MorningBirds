cd4b060fc9b7d00914de5a0f40464c2a083ffff7
Marc-Eric Boury <marc-eric.boury@collegelasalle.com>
2024-11-04 09:44:35 -0500

    demonstration merge conflict

---
+++ 420DA3_A24_Projet/Business/Domain/Client.cs
@@ -0,0 +1,16 @@
+﻿namespace _420DA3_A24_Projet.Business.Domain;
+internal class Client {
+
+    public int Id { get; set; }
+    public string Nom { get; set; }
+    public string Prenom { get; set; }
+    public string Adresse { get; set; }
+    public string Ville { get; set; }
+    public string Province { get; set; }
+    public string CodePostal { get; set; }
+    public string City { get; set; }
+
+    public Client() {
+
+    }
+}
