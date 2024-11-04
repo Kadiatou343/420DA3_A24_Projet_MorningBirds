e0c546a5b30ca0bd74232465ae539eaebcb00c53
Kadiatou <kimkd08@gmail.com>
2024-11-04 14:24:14 -0500

    Refine User et Role

--- 420DA3_A24_Projet/Business/Domain/Role.cs
+++ 420DA3_A24_Projet/Business/Domain/Role.cs
@@ -6,30 +6,73 @@ using System.Threading.Tasks;
 
 namespace _420DA3_A24_Projet.Business.Domain
 {
+    /// <summary>
+    /// Classe représentant un rôle
+    /// </summary>
     public class Role
     {
+        /// <summary>
+        /// L'identifiant du rôle pour les administrateurs
+        /// </summary>
         public const int ADMIN_ROLE_ID = 1;
+
+        /// <summary>
+        /// L'identifiant du rôle pour les employés de bureau
+        /// </summary>
         public const int OFFICE_EMPLOYEE_ROLE_ID = 2;
+
+        /// <summary>
+        /// L'identifiant du rôle pour les employés d'entrepot
+        /// </summary>
         public const int WH_EMPLOYEE_ROLE_ID = 3;
+
+        /// <summary>
+        /// Taille maximale pour le nom du rôle
+        /// </summary>
         public const int ROLE_NAME_MAX_LENGTH = 64;
+
+        /// <summary>
+        /// Taille maximale pour la description du rôle
+        /// </summary>
         public const int ROLE_DESCRIPTION_MAX_LENGTH = 255;
 
+        // Identifiant du rôle 
         public int Id {  get; set; }
+
+        // Données du rôle
         public string RoleName {  get; set; }
         public string RoleDescription { get; set; }
+
+        // Meta-données
         public DateTime DateCreated { get; set; }
         public DateTime? DateModified { get; set; }
         public DateTime? DateDeleted { get; set; }
         public byte[] RowVersion { get; set; } = null!;
 
+        // Propriétés de navigation EF Core
         public List<User> Users { get; set; } = new List<User>();
 
+        /// <summary>
+        /// Constructeur orienté création manuelle
+        /// </summary>
+        /// <param name="roleName">Le nom du rôle</param>
+        /// <param name="roleDescription">La description du rôle</param>
         public Role(string roleName, string roleDescription)
         {
             this.RoleName = roleName;
             this.RoleDescription = roleDescription;
         }
 
+        /// <summary>
+        /// Constructeur orienté création par Entity Framework
+        /// </summary>
+        /// <param name="id">l'identifiant du rôle</param>
+        /// <param name="roleName">Le nom du rôle</param>
+        /// <param name="roleDescription">La description du rôle</param>
+        /// <param name="dateCreated">La date de création du rôle</param>
+        /// <param name="dateModified">La date de modification du rôle</param>
+        /// <param name="dateDeleted">La date de suppression du rôle</param>
+        /// <param name="rowVersion">Valeur anti-concurrence de la base de données</param>
         protected Role(int id,
             string roleName,
             string roleDescription,
@@ -45,7 +88,15 @@ namespace _420DA3_A24_Projet.Business.Domain
             this.RowVersion = rowVersion;
         }
 
-        // Methodes de validation
+        #region Méthodes
+
+        /// <summary>
+        /// Override de la méthode ToString pour afficher les informations d'un rôle
+        /// </summary>
+        /// <returns>Un string représentant le rôle</returns>
+        public override string ToString() {
+            return $"#{this.Id} - {this.RoleName} - {this.RoleDescription}";
+        }
         public bool ValidateRoleName(string roleName)
         {
             return roleName.Length <= ROLE_NAME_MAX_LENGTH;
@@ -55,5 +106,7 @@ namespace _420DA3_A24_Projet.Business.Domain
         {
             return roleDescription.Length <= ROLE_DESCRIPTION_MAX_LENGTH;
         }
+
+        #endregion
     }
 }
