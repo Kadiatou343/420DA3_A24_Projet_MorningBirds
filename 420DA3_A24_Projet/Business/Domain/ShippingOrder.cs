7d8a57bc9b400c4359d25e644199db6206a85c87
abdoulaye <abdoulaye.ndiaye@lcieducation.net>
2024-11-10 00:51:08 +0100

    ajout class DAO pas encore implmté

--- 420DA3_A24_Projet/Business/Domain/ShippingOrder.cs
+++ 420DA3_A24_Projet/Business/Domain/ShippingOrder.cs
@@ -23,7 +23,7 @@ namespace _420DA3_A24_Projet.Business.Domain
         public User? CreatorEmployee { get; set; }
         public User? FulfillerEmployee { get; set; }
         public Address DestinationAddress { get; set; }
-        public List<ShippingOrderProduct> ShippingOrderProducts { get; set; }
+        public virtual List<ShippingOrderProduct> ShippingOrderProducts { get; set; }
         //constructeur de l'App
         public ShippingOrder(int CreatorEmployeeId, int DestinationAdressId) {
             this.CreatorEmployeeId = CreatorEmployeeId;
