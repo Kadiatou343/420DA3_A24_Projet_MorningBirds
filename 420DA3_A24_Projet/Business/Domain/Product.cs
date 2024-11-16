9991a97502925474f9ce7b9ff6e373c1d1138686
Kiiiiiikikiiiki <ginocad.dev@gmail.com>
2024-11-16 13:23:44 -0500

    Product and Supplier Validations Methodes + ToString() override

--- 420DA3_A24_Projet/Business/Domain/Product.cs
+++ 420DA3_A24_Projet/Business/Domain/Product.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace _420DA3_A24_Projet.Business.Domain
@@ -11,12 +13,16 @@ namespace _420DA3_A24_Projet.Business.Domain
         // Constants 
         public const int PRODUCT_NAME_MAX_LENGTH = 50;
         public const int DESC_MAX_LENGTH = 256;
-        public const int CODE_UPC_MAX_LENGTH = 13;
+        public const int CODE_UPC_MAX_LENGTH = 12;
         public const int PICTURE_NAME_MAX_LENGTH = 256;
         public const int SUPPLIER_CODE_MAX_LENGTH = 128;
 
         // Private properties 
         private string productName = null!;
+        private string desc = null!;
+        private string codeUPC = null!;
+        private string pictureName = null!;
+        private string supplierCode = null!;
 
         public int ProductId { get; set; }
         public string ProductName { get {
@@ -26,12 +32,36 @@ namespace _420DA3_A24_Projet.Business.Domain
                     ? throw new ArgumentOutOfRangeException($"Product Name must be under {PRODUCT_NAME_MAX_LENGTH} character!")
                     : value;
             } }
-        public string Desc { get; set; }
-        public string CodeUPC { get; set; }
-        public String? PictureName { get; set; }
+        public string Desc { get {
+                return this.desc;
+            } set {
+                this.desc = !ValidateProductDesc(value)
+                    ? throw new ArgumentOutOfRangeException($"Product Desc must be under {DESC_MAX_LENGTH} characters!")
+                    : value;
+            } }
+        public string CodeUPC { get {
+                return this.codeUPC;
+            } set {
+                this.codeUPC = !ValidateCodeUPC(value)
+                    ? throw new ArgumentOutOfRangeException($"Product Code UPC must be exactly {CODE_UPC_MAX_LENGTH} characters!")
+                    : value;
+            } }
+        public String? PictureName { get {
+                return this.pictureName;
+            } set {
+                this.pictureName = !ValidatePictureName(value)
+                    ? throw new ArgumentOutOfRangeException($"Product Picture Name must be under {PICTURE_NAME_MAX_LENGTH} characters!")
+                    : value;
+            } }
         public int ClientId { get; set; }
         public int SupplierId { get; set; }
-        public string SupplierCode { get; set; }
+        public string SupplierCode { get {
+                return this.supplierCode;
+            } set {
+                this.supplierCode = !ValidateSupplierCode(value)
+                    ? throw new ArgumentOutOfRangeException($"Product Supplier Code must be under {SUPPLIER_CODE_MAX_LENGTH} characters!")
+                    : value;
+            } }
         public int Quantity { get; set; }
         public int AimQuantity { get; set; }
         public double Weight { get; set; }
@@ -80,8 +110,29 @@ namespace _420DA3_A24_Projet.Business.Domain
             return value.Length <= PRODUCT_NAME_MAX_LENGTH;
         }
 
-        // TODO GINO : Finir les méthodes de validation restante.
+        static private bool ValidateProductDesc(string value) {
+            return value.Length <= DESC_MAX_LENGTH;
+        }
 
+        static private bool ValidateCodeUPC(string value) {
+            return value.Length == CODE_UPC_MAX_LENGTH;
+        }
+
+        static private bool ValidatePictureName(string value) {
+            return value.Length <= PICTURE_NAME_MAX_LENGTH || value == null;
+        }
+
+        static private bool ValidateSupplierCode(string value) {
+            return value.Length <= SUPPLIER_CODE_MAX_LENGTH;
+        }
+
+        #endregion
+
+        #region METHODES
+        
+        public override string ToString() {
+            return $"#{this.ProductId} - {this.productName} | {this.Quantity}"; 
+        }
         #endregion
 
     }
