a59d5d140ecf1a6c4e8895709e190470418f2427
Kadiatou <kimkd08@gmail.com>
2024-11-29 22:14:34 -0500

    User et Role services

---
+++ 420DA3_A24_Projet/Business/Services/RoleService.cs
@@ -0,0 +1,46 @@
+﻿using _420DA3_A24_Projet.Business.Domain;
+using _420DA3_A24_Projet.DataAccess.Contexts;
+using _420DA3_A24_Projet.DataAccess.DAOs;
+using _420DA3_A24_Projet.Presentation.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _420DA3_A24_Projet.Business.Services;
+internal class RoleService {
+
+    private readonly RoleDAO dao;
+    private readonly RoleView window;
+
+    public RoleService(WsysApplication parentApp, WsysDbContext context) {
+        this.dao = new RoleDAO(context);
+        this.window = new RoleView(parentApp);
+    }
+
+    public Role? GetRoleById(int id, bool exludeDeleted = true) {
+        return this.dao.GetById(id, exludeDeleted);
+    }
+
+    public List<Role> GetAllRoles(bool excludeDeleted = true) {
+        return this.dao.GetAll(excludeDeleted);
+    }
+
+    public Role CreateRole(Role role) {
+        return this.dao.Create(role);
+    }
+
+    public Role UpdateRole(Role role) {
+        return this.dao.Update(role);
+    }
+
+    public void DeleteRole(Role role, bool softDeleted = true) {
+        this.dao.Delete(role, softDeleted);
+    }
+
+    public List<Role> SearchRoles(string filter, bool excludeDeleted = true) {
+        return this.dao.Search(filter, excludeDeleted);
+    }
+
+}
---
+++ 420DA3_A24_Projet/Business/Services/UserService.cs
@@ -0,0 +1,56 @@
+﻿using _420DA3_A24_Projet.Business.Domain;
+using _420DA3_A24_Projet.DataAccess.Contexts;
+using _420DA3_A24_Projet.DataAccess.DAOs;
+using _420DA3_A24_Projet.Presentation.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _420DA3_A24_Projet.Business.Services;
+internal class UserService {
+    private readonly UserDAO dao;
+    private readonly UserView window;
+
+    public UserService(WsysApplication parentApp, WsysDbContext context) {
+        this.dao = new UserDAO(context);
+        this.window = new UserView(parentApp);
+    }
+
+    public User CreateUser(User user) {
+        return this.dao.Create(user);
+    }
+
+    public User UpdateUser(User user) {
+        return this.dao.Update(user);
+    }
+
+    public User? GetUserById(int id, bool excludeDeleted = true) {
+        return this.dao.GetById(id, excludeDeleted);
+    }
+
+    public User? GetByUsername(string username) {
+        return this.dao.GetByUsername(username);
+    }
+
+    public List<User> GetAllUsers(bool excludeDeleted = true) {
+        return this.dao.GetAll(excludeDeleted);
+    }
+
+    public List<User> GetUsersByWarehouse(Warehouse warehouse, bool excludeDeleted = true) {
+        return this.dao.GetByWarehouse(warehouse, excludeDeleted);
+    }
+
+    public List<User> SearchUsers(string filter, bool excludeDeleted = true) {
+        return this.dao.Search(filter, excludeDeleted);
+    }
+
+    public void DeleteUser(User user, bool softDelete = true) {
+        this.dao.Delete(user, softDelete);
+    }
+
+
+
+
+}
