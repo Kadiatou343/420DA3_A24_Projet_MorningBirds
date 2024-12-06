8e67553537fb3d33291b2655efc9aee41f692582
Kadiatou <kimkd08@gmail.com>
2024-12-06 13:27:54 -0500

    update documentation on User Role (domain)

--- 420DA3_A24_Projet/Business/Domain/User.cs
+++ 420DA3_A24_Projet/Business/Domain/User.cs
@@ -36,14 +36,26 @@ namespace _420DA3_A24_Projet.Business.Domain
         /// </summary>
         public const int PASSWORD_MAX_LENGTH = 32;
 
+        /// <summary>
+        /// Champ privé pour le nom d'utilisateur
+        /// </summary>
         private string username = null!;
+
+        /// <summary>
+        /// Champ privé pour le hash du mot de passe de l'utilisateur
+        /// </summary>
         private string passwordHash = null!;
 
-
-        // Identifiants
+        /// <summary>
+        /// L'identifiant de l'utilisateur
+        /// </summary>
         public int Id {  get; set; }
 
         // Données de l'utilisateur 
+
+        /// <summary>
+        /// Le nom d'utilisateur de l'utilisateur
+        /// </summary>
         public string Username {
             get { return this.username; }
             set {
@@ -53,6 +65,10 @@ namespace _420DA3_A24_Projet.Business.Domain
                 this.username = value;
             }
         }
+
+        /// <summary>
+        /// Le hash du mot de passe de l'utilisateur
+        /// </summary>
         public string PasswordHash {
             get { return this.passwordHash; }
             set {
@@ -62,18 +78,36 @@ namespace _420DA3_A24_Projet.Business.Domain
                 this.passwordHash = value;
             }
         }
+
+        /// <summary>
+        /// L'id de l'entrepôt associé à l'utilisateur s'il est un employé d'entrepôt
+        /// </summary>
         public int? EmployeeWarehouseId { get; set; }
 
-        // Meta-donnees 
+        /// <summary>
+        /// Date de création de l'utilisateur
+        /// </summary>
         public DateTime DateCreated { get; set; }
+
+        /// <summary>
+        /// Date de modification de l'utilisateur
+        /// </summary>
         public DateTime? DateModified { get; set; }
+
+        /// <summary>
+        /// Date de suppression de l'utilisateur
+        /// </summary>
         public DateTime? DateDeleted { get; set; }
+
+        /// <summary>
+        /// Valeur anti-concurence de EF Core
+        /// </summary>
         public byte[] RowVersion { get; set; } = null!;
 
         // Proprietes de navigation EF Core
 
         /// <summary>
-        /// L'entrepot associé à cet utilisateur s'il est un employé d'entrepot
+        /// L'entrepôt associé à cet utilisateur s'il est un employé d'entrepot
         /// </summary>
         public virtual Warehouse? EmployeeWarehouse { get; set; } = null;
 
@@ -83,12 +117,12 @@ namespace _420DA3_A24_Projet.Business.Domain
         public virtual List<Role> Roles { get; set; } = new List<Role>();
 
         /// <summary>
-        /// Les commandes d'expedition crées pas cet utilisateur s'il est employé de bureau ou admin
+        /// Les commandes d'expédition crées pas cet utilisateur s'il est employé de bureau ou admin
         /// </summary>
         public virtual List<ShippingOrder> CreatedShipOrders { get; set; } = new List<ShippingOrder>();
 
         /// <summary>
-        /// Les commandes d'expedition executés par cet utilisateur s'il est employé de bureau ou admin
+        /// Les commandes d'expédition executées par cet utilisateur s'il est employé de bureau ou admin
         /// </summary>
         public virtual List<ShippingOrder> FulfilledShipOrders { get; set; } = new List<ShippingOrder>();
 
@@ -98,7 +132,7 @@ namespace _420DA3_A24_Projet.Business.Domain
         /// </summary>
         /// <param name="username">Le nom d'utilisateur de l'utilisateur</param>
         /// <param name="passwordHash">Le Hash du mot de passe de l'utilisateur</param>
-        /// <param name="employeeWarehouseId">L'identifiant de l'entrepot associé à l'utilisateur s'il est un employé d'entrepot</param>
+        /// <param name="employeeWarehouseId">L'identifiant de l'entrepôt associé à l'utilisateur s'il est un employé d'entrepot</param>
 
         public User(string username, string passwordHash, int? employeeWarehouseId = null)
         {
@@ -113,7 +147,7 @@ namespace _420DA3_A24_Projet.Business.Domain
         /// <param name="id">L'identifiant de l'utilisateur</param>
         /// <param name="username">Le nom d'utilisateur de l'utilisateur</param>
         /// <param name="passwordHash">Le Hash du mot de passe de l'utilisateur</param>
-        /// <param name="employeeWarehouseId">L'identifiant de l'entrepot associé à l'utilisateur s'il est un employé d'entrepot</param>
+        /// <param name="employeeWarehouseId">L'identifiant de l'entrepôt associé à l'utilisateur s'il est un employé d'entrepot</param>
         /// <param name="dateCreated">La date de création de l'utilisateur</param>
         /// <param name="dateModified">La date de modification de l'utilisateur</param>
         /// <param name="dateDeleted">La date de suppression de l'utilisateur</param>
@@ -134,7 +168,7 @@ namespace _420DA3_A24_Projet.Business.Domain
             this.RowVersion = rowVersion;
         }
 
-        #region Methodes
+        #region Méthodes
 
         /// <summary>
         /// Override de la méthode ToString pour afficher les informations d'un utilisateur
@@ -143,23 +177,34 @@ namespace _420DA3_A24_Projet.Business.Domain
         public override string ToString() {
             return $"#{this.Id} - {this.Username}";
         }
+
+        /// <summary>
+        /// Valider le nom d'utilisateur
+        /// </summary>
+        /// <param name="username">La chaine à faire valider</param>
+        /// <returns>Le résultat de la validation en bool</returns>
         public bool ValidateUsername(string username)
         {
             return username.Length <= USERNAME_MAX_LENGTH 
                 && username.Length >= USERNAME_MIN_LENGTH;
         }
 
+        /// <summary>
+        /// Valider le hash du mot de passe
+        /// </summary>
+        /// <param name="passwordHash">Le hash du mot de passe à faire valider</param>
+        /// <returns>Le résultat de la validation en bool</returns>
         public bool ValidatePasswordHash(string passwordHash)
         {
             return passwordHash.Length <= PASSWORDHASH_MAX_LENGTH;
         }
 
-        public bool ValidatePassword(string password) {
-            return password.Length <= PASSWORD_MAX_LENGTH 
-                && password.Length >= PASSWORD_MIN_LENGTH;
-        }
-
         // Méthodes pour determiner les roles d'un utilisateur
+
+        /// <summary>
+        /// Determiner si l'utilisateur possède le rôle d'administrateur
+        /// </summary>
+        /// <returns>Le résultat de la verification en bool</returns>
         public bool IsAdministrator()
         {
             foreach (Role role in this.Roles)
@@ -172,6 +217,10 @@ namespace _420DA3_A24_Projet.Business.Domain
             return false;
         }
 
+        /// <summary>
+        /// Determiner si l'utilisateur possède le rôle d'employé de bureau
+        /// </summary>
+        /// <returns>Le résultat de la verification en bool</returns>
         public bool IsOfficeEmployee()
         {
             foreach(Role role in this.Roles)
@@ -184,6 +233,10 @@ namespace _420DA3_A24_Projet.Business.Domain
             return false;
         }
 
+        /// <summary>
+        /// Determiner si l'utilisateur possède le rôle d'employé d'entrepôt
+        /// </summary>
+        /// <returns>Le résultat de la verification en bool</returns>
         public bool IsWarehouseEmployee()
         {
             foreach (Role role in this.Roles)
