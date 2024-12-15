ca9b95e99a24985ea3ece27babbd08af5208f8c8
Kadiatou <kimkd08@gmail.com>
2024-12-15 15:06:24 -0500

    whEmp main menu code processing

--- 420DA3_A24_Projet/Presentation/WhEmployeeMainMenu.cs
+++ 420DA3_A24_Projet/Presentation/WhEmployeeMainMenu.cs
@@ -13,18 +13,22 @@ internal partial class WhEmployeeMainMenu : Form {
     readonly WsysApplication parentApp;
 
     /// <summary>
+    /// L'employé d'entrepôt connecté
+    /// </summary>
+    private User warehouseEmployeeLoggedIn = null!;
+
+    /// <summary>
     /// Constructeur
     /// </summary>
     /// <param name="parentApp">L'application</param>
     public WhEmployeeMainMenu(WsysApplication parentApp) {
         this.parentApp = parentApp;
-        /* Load lors de l'ouverture de la vue (got to wait for so and po services)
-        this.LoadUnassignedShippingOrdersInListBox(this.parentApp.ShippingOrderService.methodFor);
-        this.LoadProcessingShippingOrdersInListBox(this.parentApp.ShippingOrderService.methodFor);
-        this.LoadPackagedShippingOrdersInListBox(this.parentApp.ShippingOrderService.methodFor);
-        this.LoadExpectedPurchaseOrdersInListBox(this.parentApp.PurchaseOrderService.methodFor);
-        */
         this.InitializeComponent();
+        this.DeactivatteAssignToMeButton();
+        this.DeactivateSetPackagedButton();
+        this.DeactivateSetReceivedButton();
+        this.DeactivateSetShippedButton();
+        this.seeDetailsButton.Enabled = false;
     }
 
     /// <summary>
@@ -33,6 +37,15 @@ internal partial class WhEmployeeMainMenu : Form {
     /// <returns>Le résultat de l'ouverture de la vue</returns>
     public DialogResult OpenView() {
         this.greetingLabel.Text = "Bonjour " + this.parentApp.LoginService.LoggedInUser?.Username;
+        this.warehouseEmployeeLoggedIn = this.parentApp.LoginService.LoggedInUser ?? null!;
+        this.LoadUnassignedShippingOrdersInListBox(
+            this.parentApp.ShippingOrderServices.GetUnassignedSoForWhEmp(this.warehouseEmployeeLoggedIn));
+        this.LoadProcessingShippingOrdersInListBox(
+            this.parentApp.ShippingOrderServices.GetFulfilledSoForWhEmp(this.warehouseEmployeeLoggedIn));
+        this.LoadPackagedShippingOrdersInListBox(
+            this.parentApp.ShippingOrderServices.GetPackagedSoForWhEmp(this.warehouseEmployeeLoggedIn));
+        this.LoadExpectedPurchaseOrdersInListBox(
+            this.parentApp.PurchaseOrderServices.GetNewPoForWhEmp(this.warehouseEmployeeLoggedIn));
         return this.ShowDialog();
     }
 
@@ -62,8 +75,8 @@ internal partial class WhEmployeeMainMenu : Form {
     /// Recharger une liste d'ordre d'expéditions non assignés dans la list box fait pour
     /// </summary>
     private void ReloadUnassignedShippingOrders() {
-        // Got to wait for Shipping Order Service
-        //this.LoadUnassignedShippingOrdersInListBox(this.parentApp.ShippingOrderService.methodFor);
+        this.LoadUnassignedShippingOrdersInListBox(
+            this.parentApp.ShippingOrderServices.GetUnassignedSoForWhEmp(this.warehouseEmployeeLoggedIn));
     }
 
     #endregion
@@ -94,8 +107,8 @@ internal partial class WhEmployeeMainMenu : Form {
     /// Recharger une liste d'ordre d'expédition en cours de traitement dans la list box fait pour
     /// </summary>
     private void ReloadProcessingShippingOrders() {
-        // Got to wait for Shipping Order Service
-        //this.LoadProcessingShippingOrdersInListBox(this.parentApp.ShippingOrderService.methodFor);
+        this.LoadProcessingShippingOrdersInListBox(
+            this.parentApp.ShippingOrderServices.GetFulfilledSoForWhEmp(this.warehouseEmployeeLoggedIn));
     }
 
     #endregion
@@ -127,8 +140,8 @@ internal partial class WhEmployeeMainMenu : Form {
     /// Recharger une liste d'ordre d'expédition emballés dans la list box fait pour
     /// </summary>
     private void ReloadPackagedShippingOrders() {
-        // Got to wait for Shipping Order Service
-        //this.LoadPackagedShippingOrdersInListBox(this.parentApp.ShippingOrderService.methodFor);
+        this.LoadPackagedShippingOrdersInListBox(
+            this.parentApp.ShippingOrderServices.GetPackagedSoForWhEmp(this.warehouseEmployeeLoggedIn));
     }
 
     #endregion
@@ -159,8 +172,8 @@ internal partial class WhEmployeeMainMenu : Form {
     /// Recharger une liste d'ordre de restockage attentus dans la list box fait pour
     /// </summary>
     private void ReloadExpectedPurchaseOrders() {
-        // Got to wait for Purchase Order Service
-        //this.LoadExpectedPurchaseOrdersInListBox(this.parentApp.PurchaseOrderService.methodFor);
+        this.LoadExpectedPurchaseOrdersInListBox(
+            this.parentApp.PurchaseOrderServices.GetNewPoForWhEmp(this.warehouseEmployeeLoggedIn));
     }
 
     #endregion
@@ -228,5 +241,77 @@ internal partial class WhEmployeeMainMenu : Form {
     private void Logout_Click(object sender, EventArgs e) {
         this.parentApp.LoginService.Logout();
         this.DialogResult = DialogResult.Continue;
+    }
+
+    private void SoUnassignedRefreshBtn_Click(object sender, EventArgs e) {
+        this.ReloadUnassignedShippingOrders();
+    }
+
+    private void SoPackagedRefreshBtn_Click(object sender, EventArgs e) {
+        this.ReloadPackagedShippingOrders();
+    }
+
+    private void PoRefreshBtn_Click(object sender, EventArgs e) {
+        this.ReloadExpectedPurchaseOrders();
+    }
+
+    private void SoProcessingRefreshBtn_Click(object sender, EventArgs e) {
+        this.ReloadProcessingShippingOrders();
+    }
+
+    private void UnassignedSoListBox_SelectedIndexChanged(object sender, EventArgs e) {
+        ShippingOrder? selectedSo = this.unassignedSoListBox.SelectedItem as ShippingOrder;
+
+        if (selectedSo != null) {
+            this.ActivateAssignToMeButton();
+        } else {
+            this.DeactivatteAssignToMeButton();
+        }
+    }
+
+    private void ProcessingSoListBox_SelectedIndexChanged(object sender, EventArgs e) {
+        ShippingOrder? selectedSo = this.processingSoListBox.SelectedItem as ShippingOrder;
+
+        if (selectedSo != null) {
+            this.ActivateSetPackagedButton();
+            this.seeDetailsButton.Enabled = true;
+        } else {
+            this.seeDetailsButton.Enabled = false;
+            this.DeactivateSetPackagedButton();
+        }
+    }
+
+    private void PackagedSoListBox_SelectedIndexChanged(object sender, EventArgs e) {
+        ShippingOrder? selectedSo = this.packagedSoListBox.SelectedItem as ShippingOrder;
+
+        if (selectedSo != null) {
+            this.ActivateSetShippedButton();
+        } else {
+            this.DeactivateSetShippedButton();
+        }
+    }
+
+    private void ExpectedPoListBox_SelectedIndexChanged(object sender, EventArgs e) {
+        
+    }
+
+    private void AssignToMeButton_Click(object sender, EventArgs e) {
+
+    }
+
+    private void SeeDetailsButton_Click(object sender, EventArgs e) {
+
+    }
+
+    private void SetPackagedButton_Click(object sender, EventArgs e) {
+
+    }
+
+    private void SetShippedButton_Click(object sender, EventArgs e) {
+
+    }
+
+    private void SetReceivedButton_Click(object sender, EventArgs e) {
+
     }
 }
